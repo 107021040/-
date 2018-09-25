05e804cb45414684c78560c34ef493a86e42b4f6
107021040 <43198583+107021040@users.noreply.github.com>
2018-09-25 17:36:09 +0800

    Add files via upload
    P01

--- Main.java
+++ Main.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int v1 = scn.nextInt();
-        int v2 = scn.nextInt();
-        System.out.println(v1 + v2);
-        System.out.println(v1 - v2);
-        System.out.println(v1 * v2);
+	Scanner scn = new Scanner(System.in);
+	int v1 = scn.nextInt();
+	System.out.println(v1*9/5+32);
     }
 }
