30826eae525d5f21485f1c28c02a777c05db6dbd
107021040 <43198583+107021040@users.noreply.github.com>
2018-09-25 17:41:24 +0800

    Add files via upload
    P06

--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scn = new Scanner(System.in);
-	int v1 = scn.nextInt();
-	System.out.println(v1*9/5+32);
+        Scanner scn = new Scanner(System.in);
+        int v1 = scn.nextInt();
+        if ((v1 % 4 == 0 && v1 % 100 != 0) || (v1 % 400 == 0)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
